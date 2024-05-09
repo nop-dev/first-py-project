cba0f649e531d2541b99d2bba36c6e988e3f5b02
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-05-08 21:25:33 -0300

    feat: :sparkles: Adiciona parte funcional do jogo...

--- main.py
+++ main.py
@@ -13,4 +13,16 @@ palavra_secreta = valor_secreto['palavra']
 dica = valor_secreto['dica']
 
 # apresenta as informações
-print(f'Esta palavra tem {len(palavra_secreta)} letras. A dica é: {dica}')
+# print(f'Esta palavra tem {len(palavra_secreta)} letras. A dica é: {dica}')
+
+# parte funcional do joguinho
+print(f'Esta palavra tem {len(palavra_secreta)} letras.')
+print(f'A dica é -> {dica}')
+chute = input('Qual o seu chute:')
+
+if(chute == palavra_secreta):
+    print('Você acertou!')
+else:
+    print(f'Você errouuu! A palavra certa era {palavra_secreta}...')
+
+# esse foi o projetinho...
