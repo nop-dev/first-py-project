356349d72eb9c4db976d563eb06697d64b063457
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-05-08 21:14:54 -0300

    feat: :sparkles: organiza código

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+#importações
 import requests
+import random
 
+# obtendo os dados
 url = 'https://raw.githubusercontent.com/guilhermeonrails/api-imersao-ia/main/words.json'
-
 resposta = requests.get(url)
 data = resposta.json()
 
-print(len(data))
+# organizando os dados
+valor_secreto = random.choice(data)
+palavra_secreta = valor_secreto['palavra']
+dica = valor_secreto['dica']
 
-print(data[0])
+# apresenta as informações
+print(f'Esta palavra tem {len(palavra_secreta)} letras. A dica é: {dica}')
